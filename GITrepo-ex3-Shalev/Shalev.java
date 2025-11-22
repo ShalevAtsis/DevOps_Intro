5e2cab48a4a00dc32bd0b57bb1d275b5cd810da6
Shalev Atsis <shalevatsis@gmail.com>
2025-11-22 19:15:49 +0200

    test

--- GITrepo-ex3-Shalev/Shalev.java
+++ GITrepo-ex3-Shalev/Shalev.java
@@ -1,4 +1,4 @@
-public class shalev {
+public class Shalev {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Shalev Atsis... " + i);
