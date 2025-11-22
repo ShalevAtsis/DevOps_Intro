a0fcdd08de2203963c5128cd30d6a38c7c6793bd
Shalev Atsis <shalevatsis@gmail.com>
2025-11-22 18:49:19 +0200

    Update Hello.java

--- GITrepo-ex3-Shalev/Hello.java
+++ GITrepo-ex3-Shalev/Hello.java
@@ -1,7 +1,5 @@
-public class Hello{ 
-	public static void main(String[] args) { 	
-		for (int i=1;i<=10; i++) { 
-			System.out.println("Helloooooooo World ..."+i); 
-		} 
-	} 
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
