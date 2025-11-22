08dc0e820b4cbb4b9fdbef849aca9543a1a3a1f4
Shalev Atsis <shalevatsis@gmail.com>
2025-11-22 18:16:10 +0200

    git commit job trigger test

--- GITrepo-ex3-Shalev/shalev.java
+++ GITrepo-ex3-Shalev/shalev.java
@@ -1,7 +1,7 @@
 public class shalev{ 
 	public static void main(String[] args) { 	
 		for (int i=1;i<=10; i++) { 
-			System.out.println("Shalev ..."+i); 
+			System.out.println("Shalev Atsis..."+i); 
 		} 
 	} 
 }
